2ba7b6927c18fd002a8bd171f5ad3b91070ed8c4
jazztinecruz <jzztn.crz@gmail.com>
2022-01-21 18:34:24 +0800

    update

--- components/instructors/Header.jsx
+++ components/instructors/Header.jsx
@@ -1,9 +1,17 @@
 import { LogoutIcon, UploadIcon } from '@heroicons/react/outline'
 import Link from 'next/link';
+import { useState } from 'react';
 
 const Header = ({ name, position }) => {
+
+	const [upload, setUpload] = useState(false)
+
+	const handleUpload = () => {
+		setUpload(!upload)
+	}
+
 	return (
-		<nav className="py-5 px-6">
+		<nav className="relative py-5 px-6">
 			<div>
 				<div className="flex gap-6 justify-end">
 
@@ -12,7 +20,15 @@ const Header = ({ name, position }) => {
 						<div className='bg-gradient-to-b from-pink to-violet w-0 h-1 rounded-md group-hover:w-full transition-all duration-500'></div>
 					</div>
 
-					<UploadIcon className='w-6 h-6 hover:text-gray transition-all duration-700 cursor-pointer' />
+					<div>
+						<UploadIcon className='w-6 h-6 hover:text-gray transition-all duration-700 cursor-pointer' onClick={handleUpload} />
+						<div className={`absolute top-15 right-5 w-96 bg-white border-[1px] border-gray py-4 px-6 grid gap-8 rounded-md ${upload ? 'hidden' : 'block'}`}>
+							<div className="w-ful border-[1px] border-gray-300 py-3 pl-4 tracking-wide rounded-md">Choose a File</div>
+							<div>
+								<button className='bg-gradient-to-b from-violet to-pink py-3 px-10 text-white rounded-md'>Upload</button>
+							</div>
+						</div>
+					</div>
 					<Link href={'/'}><LogoutIcon className='w-6 h-6 hover:text-gray transition-all duration-700 cursor-pointer' /></Link>
 				</div>
 			</div>
